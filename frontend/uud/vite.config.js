798c49a6738f117e714eac094f7a0f0a06af0c99
jkuzmych <92061086+jkuzmych@users.noreply.github.com>
2025-12-31 18:23:32 -0600

    vite.config.js edited

--- frontend/uud/vite.config.js
+++ frontend/uud/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Ukraine-Unemployment-Report/frontend/uud/" ,
+  base: "/Ukraine-Unemployment-Report/",
   plugins: [
     react(),
   ],
